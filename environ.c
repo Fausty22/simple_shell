0bf24d79f84c3ad131e43f6384d871bf6b5820fc
Fausty22 <faustinanwokolo@gmail.com>
2024-02-28 18:56:39 -0800

    Updated

---
+++ environ.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * _myenv - Printings the likely environment
+ * @info: Structuring Contains noumenon argumentation. Use in maintaining
+ *          steady function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - Getting value of the environ variable
+ * @info: Structuring contains noumenon argumentations. Using to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialising the new environment changeable,
+ *             or revamp an existence one
+ * @info: Structuring contains noumenon argumentations. Using to maintain
+ *        steady function prototypes.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Removing an environmental changeable
+ * @info: Structurings contains noumenon argumentations. Using to maintain
+ *        Steady function prototypes.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - Populating the envi linked list
+ * @info: Structurings contains noumenon argumentations. Use in maintaining
+ *          steady function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ *_eputs - Printing the input strings
+ * @str: The strings meant to print
+ *
+ * Return: insignificant
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - Writing the characters c to stderr
+ * @c: Characters to be printed
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set orderly.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - Writing the characters c to the given fd
+ * @c: The Characters to be printed
+ * @fd: The filedescriptor to be written in
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - Printings in an input string
+ * @str: The strings meant to be printed
+ * @fd: The filedescriptor to be written to
+ *
+ * Return: The numbers of characters placed
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
