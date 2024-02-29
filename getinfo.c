7ddbe31bbbd4d816d31027cd8ac22800e9ac3011
Fausty22 <faustinanwokolo@gmail.com>
2024-02-28 20:15:40 -0800

    Updated

--- getinfo.c
+++ getinfo.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * clear_info - Assigning info_t structural
- * @info: structurally address
+ * clear_info - initializes info_t struct
+ * @info: struct address
  */
 void clear_info(info_t *info)
 {
@@ -13,9 +13,9 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - Assigning info_t structurlly
- * @info: Structuring address
- * @av: Argumentative vector
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
  */
 void set_info(info_t *info, char **av)
 {
@@ -45,9 +45,9 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - freeings info_t structural fields
- * @info: Structing Address
- * @all: True incase freeing all fields
+ * free_info - frees info_t struct fields
+ * @info: struct address
+ * @all: true if freeing all fields
  */
 void free_info(info_t *info, int all)
 {
